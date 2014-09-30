b4a3961bcf29838bb57ddb6ed9de3307cfd4242d
Justin Cuaresma <justincuaresma@gmail.com>
2014-09-30 11:57:26 -0400

    [Ch24 - update] Added TableLayout + wired
    Added:
    - Layout with TableLayout as root
    - Wired up components to listeners

--- gen/com/atleusdigital/android/remotecontrol/R.java
+++ gen/com/atleusdigital/android/remotecontrol/R.java
@@ -24,12 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080007;
         public static final int fragmentContainer=0x7f080000;
+        public static final int fragment_remote_control_enterButton=0x7f080006;
+        public static final int fragment_remote_control_oneButton=0x7f080005;
+        public static final int fragment_remote_control_selected-TextView=0x7f080002;
+        public static final int fragment_remote_control_tableLayout=0x7f080001;
+        public static final int fragment_remote_control_working-TextView=0x7f080003;
+        public static final int fragment_remote_control_zeroButton=0x7f080004;
     }
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
-        public static final int activity_remote_control=0x7f030001;
+        public static final int fragment_remote_control=0x7f030001;
     }
     public static final class menu {
         public static final int remote_control=0x7f070000;
--- src/com/atleusdigital/android/remotecontrol/RemoteControlFragment.java
+++ src/com/atleusdigital/android/remotecontrol/RemoteControlFragment.java
@@ -1,7 +1,64 @@
 package com.atleusdigital.android.remotecontrol;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RemoteControlFragment extends Fragment {
-
+	private TextView mSelectedTextView;
+	private TextView mWorkingTextView;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.activity_fragment, container, false);
+		
+		mSelectedTextView = (TextView)v.findViewById(R.id.fragment_remote_control_selected);
+		
+		mWorkingTextView = (TextView)v.findViewById(R.id.fragment_remote_control_working);
+		
+		View.OnClickListener numberButtonListener = new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TextView textView = (TextView)v;
+				String working = mWorkingTextView.getText().toString();
+				String text = textView.getText().toString();
+				if (working.equals("0")) {
+					mWorkingTextView.setText(text);
+				} else {
+					mWorkingTextView.setText(working + text);
+				}
+			}
+		};
+		
+		Button zeroButton = (Button)v.findViewById(R.id.fragment_remote_control_zeroButton);
+		zeroButton.setOnClickListener(numberButtonListener);
+		
+		Button oneButton = (Button)v.findViewById(R.id.fragment_remote_control_oneButton);
+		oneButton.setOnClickListener(numberButtonListener);
+		
+		Button enterButton = (Button)v.findViewById(R.id.fragment_remote_control_enterButton);
+		enterButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				CharSequence working = mWorkingTextView.getText();
+				if (working.length() > 0) {
+					mSelectedTextView.setText(working);
+					mWorkingTextView.setText("0");
+				}
+			}
+		});
+		
+		return v;
+	}
+	
+	
+	
 }
