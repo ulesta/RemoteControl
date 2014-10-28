aedf2caf708fea8a27062a8c01241883cffb075f
Justin Cuaresma <justincuaresma@gmail.com>
2014-10-28 13:26:07 -0400

    [Ch24 - update] Added LayerList to add shadow
    Added:
    + created a LayerList which nests both a rect shape and an Inset which
    allows to inset a specified drawable, in this case: button_shape. This
    allows us to create a shadow effect on the buttons

--- gen/com/atleusdigital/android/remotecontrol/R.java
+++ gen/com/atleusdigital/android/remotecontrol/R.java
@@ -24,8 +24,10 @@ public final class R {
         public static final int button_shape=0x7f020000;
         public static final int button_shape_normal=0x7f020001;
         public static final int button_shape_pressed=0x7f020002;
-        public static final int button_text_color=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
+        public static final int button_shape_shadowed=0x7f020003;
+        public static final int button_text_color=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int remote_background=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f080004;
